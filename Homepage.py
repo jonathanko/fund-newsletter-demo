5afd747c2813d0938df7f92f98ded7340396fb3b
jonathanko <4978523+jonathanko@users.noreply.github.com>
2025-05-29 17:53:29 +0800

    Add files via upload

--- Homepage.py
+++ Homepage.py
@@ -17,11 +17,19 @@ st.set_page_config(
     }
 )
 
+st.sidebar.image("logo.png", width=500)
 st.sidebar.title("Table of Contents")
+st.sidebar.markdown("[Overview](#overview)")
+st.sidebar.markdown("[Strategy Description](#strategy-description)")
+st.sidebar.markdown("[Portfolio Performance](#portfolio-performance)")
+st.sidebar.markdown("[Market Commentary](#market-commentary)")
+st.sidebar.markdown("[Risk & Returns](#risk-and-returns)")
+st.sidebar.markdown("[Portfolio Exposure](#portfolio-exposure)")
+st.sidebar.markdown("[Terms of the Fund](#terms-of-the-fund)")
+st.sidebar.markdown("[Contact Details](#contact-details)")
+st.sidebar.markdown("[Disclaimer](#disclaimer)")
 
 st.title("Monthly Update | Fund April 2025")
-
-
 
 #st.button("Contact Us", key="contact_us", help="Contact us for more information")
 
@@ -30,80 +38,87 @@ st.title("Monthly Update | Fund April 2025")
 col1, col2 = st.columns(2)
 col1, col2 = st.columns([1, 1])  #column widths ratios
 
+
 with col1:
-    st.markdown(" ")
     st.header("Overview")
+    col1a, col1b = st.columns(2)
+    col1a, col1b = st.columns([1, 3])  #column widths ratios
+    with col1a:
+        st.markdown("Bloomberg Ticker:")
+        st.markdown("Prime Brokers:")
+        st.markdown("Fund Administrator:")
+        st.markdown("Auditor:")
+        st.markdown("Regulatory:")
+        st.markdown("Inception Date:")
+    with col1b:
+        st.markdown("TELLGNT KY Equity")
+        st.markdown("Goldman Sachs & J.P. Morgan")
+        st.markdown("State Street")
+        st.markdown("Ernst & Young")
+        st.markdown("US SEC registered and Hong Kong SFC licensed")
+        st.markdown("August 1, 2004")
+
+with col2:
+    st.header("Strategy Description")
     with open("description.txt", "r", encoding="utf-8") as file:
         text = file.read()
     st.markdown(text)
 
-with col2:
-    st.markdown(" ")
-    with open("terms.txt", "r", encoding="utf-8") as file:
-        text = file.read()
-    st.markdown(text)
+st.markdown(" ")
+st.markdown(" ")
+
+st.header("Portfolio Performance")
 
 col1, col2 = st.columns(2)
 col1, col2 = st.columns([1, 1])  #column widths ratios
 
 with col1:
+    st.subheader("Quick Update")
     flourish_iframe = """
     <div class="flourish-embed flourish-table" data-src="visualisation/23411515"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/23411515/thumbnail" width="100%" alt="table visualization" /></noscript></div>
     """
-    components.html(flourish_iframe, height=230)
+    components.html(flourish_iframe, height=190)
 
 with col2:
+    st.subheader("Contribution Breakdown")
     flourish_iframe = """
     <iframe src='https://flo.uri.sh/visualisation/23434228/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/23434228/?utm_source=embed&utm_campaign=visualisation/23434228' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
     """
-    components.html(flourish_iframe, height=230)
+    components.html(flourish_iframe, height=190)
 
-
-st.header("Performance Data")
-
-with st.sidebar:
-    st.markdown("Performance Line Chart")
+st.subheader("Growth of $100 Since Inception")
 
 df = pd.read_csv(r"data.csv")
                 
-df = df.set_index('Date')
+df = df.set_index('Month')
 
-line = px.line(df, labels={'varible':''})
+fig = px.line(df, labels={'varible':''})
 
-line = line.update_layout(
+fig = fig.update_layout(
     title='',
     template='plotly_white',
+    plot_bgcolor='white',   # Background inside the plotting area
+    paper_bgcolor='white',       # Background of the entire figure
     margin=dict(l=10, r=10, t=30, b=10),
-    xaxis_title="Date",
+    xaxis_title="Time Period",
     yaxis_title="Performance",
     height=500,
     width=2000,
     )
 
-line.data[0].line.color = 'rgb(0,128,0)'
-line.data[1].line.color = 'rgb(211,211,211)'  # Grey color for the index line
 
-line
+fig.update_xaxes(tick0=5, dtick=12)
 
-st.markdown(" ")
+#x_vals = fig.data[0].x  # assuming first trace's x-values
+#tickvals = x_vals[::12]  # every 5th value
+#ticktext = [str(val) for val in tickvals]
+#fig.update_xaxes(tickvals=tickvals, ticktext=ticktext)
 
-st.header("Risk & Returns")
-st.markdown("Risk-free rate = 3 month T-Bill")
 
-col1, col2 = st.columns(2)
-col1, col2 = st.columns([1, 1])  #column widths ratios
+fig.data[0].line.color = 'rgb(0,128,0)'
+fig.data[1].line.color = 'rgb(211,211,211)'  # Grey color for the index line
 
-with col1:
-    flourish_iframe = """
-    <div class="flourish-embed flourish-chart" data-src="visualisation/23412618"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/23412618/thumbnail" width="100%" alt="chart visualization" /></noscript></div>    
-    """
-    components.html(flourish_iframe, height=195)
-
-with col2:
-    flourish_iframe = """
-    <div class="flourish-embed flourish-chart" data-src="visualisation/23412923"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/23412923/thumbnail" width="100%" alt="chart visualization" /></noscript></div>    
-    """
-    components.html(flourish_iframe, height=195)
+fig
 
 st.markdown(" ")
 
@@ -122,7 +137,28 @@ with col2:
 
 st.markdown(" ")
 
-st.header("Exposure")
+st.header("Risk & Returns")
+st.markdown("Risk-free rate = 3 month T-Bill")
+
+col1, col2 = st.columns(2)
+col1, col2 = st.columns([1, 1])  #column widths ratios
+
+with col1:
+    flourish_iframe = """
+    <iframe src='https://flo.uri.sh/visualisation/23412618/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:200px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/23412618/?utm_source=embed&utm_campaign=visualisation/23412618' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+    """
+    components.html(flourish_iframe, height=195)
+
+with col2:
+    flourish_iframe = """
+    <iframe src='https://flo.uri.sh/visualisation/23412923/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:200px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/23412923/?utm_source=embed&utm_campaign=visualisation/23412923' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
+    """
+    components.html(flourish_iframe, height=195)
+
+st.markdown(" ")
+st.markdown(" ")
+
+st.header("Portfolio Exposure")
 
 col1, col2 = st.columns(2)
 col1, col2 = st.columns([1, 1])  #column widths ratios
@@ -141,47 +177,48 @@ with col2:
 
 st.markdown(" ")
 
-st.header("Gross Exposure")
-
 col1, col2 = st.columns(2)
 col1, col2 = st.columns([1, 1])  #column widths ratios
 
 with col1:
     flourish_iframe = """
-    <div class="flourish-embed flourish-chart" data-src="visualisation/23433368"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/23433368/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
+    <iframe src='https://flo.uri.sh/visualisation/23433368/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/23433368/?utm_source=embed&utm_campaign=visualisation/23433368' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>    
     """
     components.html(flourish_iframe, height=580)
 
 with col2:
     flourish_iframe = """
-    <div class="flourish-embed flourish-chart" data-src="visualisation/23433406"><script src="https://public.flourish.studio/resources/embed.js"></script><noscript><img src="https://public.flourish.studio/visualisation/23433406/thumbnail" width="100%" alt="chart visualization" /></noscript></div>
+    <iframe src='https://flo.uri.sh/visualisation/23433406/embed' title='Interactive or visual content' class='flourish-embed-iframe' frameborder='0' scrolling='no' style='width:100%;height:600px;' sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'></iframe><div style='width:100%!;margin-top:4px!important;text-align:right!important;'><a class='flourish-credit' href='https://public.flourish.studio/visualisation/23433406/?utm_source=embed&utm_campaign=visualisation/23433406' target='_top' style='text-decoration:none!important'><img alt='Made with Flourish' src='https://public.flourish.studio/resources/made_with_flourish.svg' style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a></div>
     """
     components.html(flourish_iframe, height=580)
 
-st.markdown("---")
-
+st.markdown(" ")
 st.header("Terms of the Fund")
 
 col1, col2 = st.columns(2)
-col1, col2 = st.columns([1, 10])  #column widths ratios
+col1, col2 = st.columns([1, 5])  #column widths ratios
 
 with col1:
-    st.markdown("Structure")
-    st.markdown("Initial Allocation")
-    st.markdown("Fees")
-    st.markdown("Prime Brokers")
-    st.markdown("Auditor")
-    st.markdown("Regulatory")
+    st.markdown("Legal Structure:")
+    st.markdown("Initial Allocation:")
+    st.markdown("Fee Structure:")
+    st.markdown("High-water Mark Protection:")
+    st.markdown("Subscription Fee:")
+    st.markdown("Withdrawal Fee:")
+    st.markdown("Withdrawal Notice Period:")
+    st.markdown("Liquidity:")
+
 with col2:
     st.markdown("Master Feeder, Cayman Ltd and Delaware LP")
     st.markdown("USD 1 million")
     st.markdown("1.5% / 20%")
-    st.markdown("Goldman Sachs and J.P. Morgan")
-    st.markdown("Ernst & Young")
-    st.markdown("US SEC registered and Hong Kong SFC licensed")
-
-st.markdown("---")
-
+    st.markdown("Yes, applies to all investors")
+    st.markdown("None")
+    st.markdown("None")
+    st.markdown("30 days")
+    st.markdown("Monthly")
+    
+st.markdown(" ")
 st.header("Contact Details")
 
 col1, col2 = st.columns(2)
